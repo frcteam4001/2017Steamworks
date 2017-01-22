9ec0216e121e3c7d6d7d707476bab48aa9d88ee5
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-01-21 20:04:29 -0500

    created electrical constants class for universal access to wiring ports.

---
+++ src/org/usfirst/frc/team4001/robot/ElectricalConstants.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team4001.robot;
+
+/**
+ * @author eric
+ *
+ * ElectricalConstants is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class ElectricalConstants {
+	
+	// ************************************************************************
+	// **************************  GEARDROP  **********************************
+	// ************************************************************************
+	
+	public static final int GEARDROP_MOTOR_LEFT			= -1;  // to be set
+	public static final int GEARDROP_MOTOR_RIGHT 		= -1;  // to be set
+	
+	public static final int GEARDROP_SWITCH_LEFT		= -1;  // to be set
+	public static final int GEARDROP_SWITCH_RIGHT		= -1;  // to be set
+	
+	public static final int GEARDROP_SOLENOID_LEFT		= -1;  // to be set
+	public static final int GEARDROP_SOLENOID_RIGHT		= -1;  // to be set
+	
+	
+}
