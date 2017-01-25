c03090d6b6d5cd11899036cc879a8a3156c0f573
4001 <4001@10.130.176.217>
2017-01-25 09:23:43 -0500

    set drivetrain and geardrop constants

--- src/org/usfirst/frc/team4001/robot/ElectricalConstants.java
+++ src/org/usfirst/frc/team4001/robot/ElectricalConstants.java
@@ -17,14 +17,28 @@ public class ElectricalConstants {
 	// **************************  GEARDROP  **********************************
 	// ************************************************************************
 	
-	public static final int GEARDROP_MOTOR_LEFT			= -1;  // to be set
-	public static final int GEARDROP_MOTOR_RIGHT 		= -1;  // to be set
+	public static final int GEARDROP_MOTOR_LEFT			= 7;  // CAN
+	public static final int GEARDROP_MOTOR_RIGHT 		= 6;  // CAN
 	
-	public static final int GEARDROP_SWITCH_LEFT		= -1;  // to be set
-	public static final int GEARDROP_SWITCH_RIGHT		= -1;  // to be set
+	public static final int GEARDROP_SWITCH_LEFT		= 0;  // DIO
+	public static final int GEARDROP_SWITCH_RIGHT		= 1;  // DIO
 	
 	public static final int GEARDROP_FENCE_LEFT			= -1;  // to be set
 	public static final int GEARDROP_FENCE_RIGHT		= -1;  // to be set
+	public static final int GEARDROP_ROLLER 			= 5;   // CAN
+	
+	// ************************************************************************
+	// **************************  DRIVETRAIN  ********************************
+	// ************************************************************************
+	public static final int DRIVETRAIN_FRONT_LEFT 				= 3;  // CAN
+	public static final int DRIVETRAIN_FRONT_RIGHT 				= 1;  // CAN
+	public static final int DRIVETRAIN_REAR_LEFT				= 4;  // CAN
+	public static final int DRIVETRAIN_REAR_RIGHT 				= 2;  // CAN
+	
+	public static final int DRIVETRAIN_ULTRASONIC_LEFT			= 0;  // ANALOG
+	public static final int DRIVETRAIN_ULTRASONIC_RIGHT 		= 1;  // ANALOG
 	
 	
+	
+
 }
