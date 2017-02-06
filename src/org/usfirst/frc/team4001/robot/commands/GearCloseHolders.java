eabb48a0fb4897205da05431b810d2d5616c1e7e
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-02-05 21:49:22 -0500

    GearSlidetoZone Command

--- src/org/usfirst/frc/team4001/robot/commands/GearCloseHolders.java
+++ src/org/usfirst/frc/team4001/robot/commands/GearCloseHolders.java
@@ -39,6 +39,8 @@ public class GearCloseHolders extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.geardrop.set_closed(true);
+
     }
 
     // Called when another command which requires one or more of the same
