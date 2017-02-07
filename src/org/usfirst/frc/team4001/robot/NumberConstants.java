3462a19285bff489468300a7d689b67d9f1cc2b2
arminale <arminale@yahoo.com>
2017-02-07 14:36:05 -0500

    update

--- src/org/usfirst/frc/team4001/robot/NumberConstants.java
+++ src/org/usfirst/frc/team4001/robot/NumberConstants.java
@@ -20,7 +20,7 @@ public class NumberConstants {
 	public static final double geardrop_holder_close_i					 = 0.0;
 	public static final double geardrop_holder_close_d					 = 0.3;
 	public static final double geardrop_holder_close_f					 = 0.0;
-	public static final int geardrop_holder_close_error					 = 10;
+	public static final int geardrop_holder_close_error					 = 50;
 	public static final int geardrop_holder_close_position				 = 15200; //pulses
 
 	
--- src/org/usfirst/frc/team4001/robot/commands/PlaceGear.java
+++ src/org/usfirst/frc/team4001/robot/commands/PlaceGear.java
@@ -1,35 +1,35 @@
-//package org.usfirst.frc.team4001.robot.commands;
-//
-//import edu.wpi.first.wpilibj.command.CommandGroup;
-//import org.usfirst.frc.team4001.robot.Robot;
-//import org.usfirst.frc.team4001.commands.auto.DriveCommand;
-//import org.usfirst.frc.team4001.robot.NumberConstants;
-//
-///**
-// *This is THE command group for placing the gear. It pulls together all other commands to handle
-// *the entire process, from start to finish. This should be the only command that is mapped to 
-// *the controller for the purpose of placing the gear. 
-// */
-//public class PlaceGear extends CommandGroup {
-//
-//    public PlaceGear() {
-//        // Add Commands here:
-//        // e.g. addSequential(new Command1());
-//        //      addSequential(new Command2());
-//        // these will run in order.
-//
-//        // To run multiple commands at the same time,
-//        // use addParallel()
-//        // e.g. addParallel(new Command1());
-//        //      addSequential(new Command2());
-//        // Command1 and Command2 will run in parallel.
-//
-//        // A command group will require all of the subsystems that each member
-//        // would require.
-//        // e.g. if Command1 requires chassis, and Command2 requires arm,
-//        // a CommandGroup containing them would require both the chassis and the
-//        // arm.
-//    	
+package org.usfirst.frc.team4001.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team4001.robot.Robot;
+import org.usfirst.frc.team4001.commands.auto.DriveCommand;
+import org.usfirst.frc.team4001.robot.NumberConstants;
+
+/**
+ *This is THE command group for placing the gear. It pulls together all other commands to handle
+ *the entire process, from start to finish. This should be the only command that is mapped to 
+ *the controller for the purpose of placing the gear. 
+ */
+public class PlaceGear extends CommandGroup {
+
+    public PlaceGear() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	
 //    	//Level to airship
 //    	addSequential(new Align());
 //    	
@@ -40,25 +40,25 @@
 //    	addSequential(new DriveCommand(distanceToTravel, 0.5, 0, 5));
 //    	
 //    	
-//    	//Wait a bit to ensure that the gear zone in the network table has been updated
-//    	try {
-//			Thread.sleep(300);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//    	
-//    	if (Robot.gearZone == -1) { //gear impossible
-//    		// drive away?
-//    	} else {
-//    		// set gear position
-//    		// go forward (how much?)
-//    		
+    	//Wait a bit to ensure that the gear zone in the network table has been updated
+    	try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	if (Robot.gearZone == -1) { //gear impossible
+    		// drive away?
+    	} else {
+    		// set gear position
+    		// go forward (how much?)
+    		
 //    		while (Robot.geardrop.gearIsInside()) {
 //    			// wait until the gear has been removed
 //    		}
-//    		// drive away (how much?)
-//    	}
-//    	
-//    }
-//}
+    		// drive away (how much?)
+    	}
+    	
+    }
+}
