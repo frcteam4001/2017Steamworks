52f1d875b6161927cf308c1b13cc326261bc581f
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2017-02-13 15:58:51 -0500

    update

--- src/org/usfirst/frc/team4001/robot/commands/PlaceGear.java
+++ src/org/usfirst/frc/team4001/robot/commands/PlaceGear.java
@@ -52,7 +52,7 @@ public class PlaceGear extends CommandGroup {
 		}
     	if (Robot.gearZone != -1 || Robot.gearZone == -20000 || Robot.gearZone == -30000){
     		addSequential(new GearSlidetoZone());
-    		addSequential(new DriveCommand(NumberConstants.camera_capture_distance - 13, 0.45, 0, 1.75, 0.3));
+    		addSequential(new DriveCommand(NumberConstants.camera_capture_distance - 12, 0.45, 0, 1.75, 0.3));
     	}
     	
     }
