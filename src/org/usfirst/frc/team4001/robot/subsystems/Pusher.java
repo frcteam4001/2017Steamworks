c556f6864358ebf4fddb8c0b16a5591411a3ad40
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2017-03-03 14:52:38 -0500

    change Pusher to 2x Solenoid

--- src/org/usfirst/frc/team4001/robot/subsystems/Pusher.java
+++ src/org/usfirst/frc/team4001/robot/subsystems/Pusher.java
@@ -15,13 +15,11 @@ public class Pusher extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
-	//.private DoubleSolenoid gear_sol;
 	private Solenoid gearPusherOut;
 	private Solenoid gearPusherIn;
 	private DigitalInput pusher_switch;
 	
 	public Pusher() {
-		//gear_sol = new DoubleSolenoid(ElectricalConstants.GEARDROP_PUSHER_OUT, ElectricalConstants.GEARDROP_PUSHER_IN);
 		
 		gearPusherOut = new Solenoid(ElectricalConstants.GEARDROP_PUSHER_OUT);
 		gearPusherIn = new Solenoid(ElectricalConstants.GEARDROP_PUSHER_IN);
@@ -29,24 +27,10 @@ public class Pusher extends Subsystem {
 	}
 	
 	
-	/*
-	public void forwardSol() {
-		gear_sol.set(DoubleSolenoid.Value.kForward);
-		
-	}
 	
-	public void reverseSol() {
-		gear_sol.set(DoubleSolenoid.Value.kReverse);
-	}
-	
-	public void stopSol() {
-		gear_sol.set(DoubleSolenoid.Value.kOff);
-	}
-	*/
-	
-	public boolean pusherPressed(){
+	public boolean pusherIsBack(){
 		// boolean return for calibrationSwitch
-		return !pusher_switch.get();
+		return pusher_switch.get();
 	}
 
     public void initDefaultCommand() {
@@ -55,13 +39,19 @@ public class Pusher extends Subsystem {
     }
     
     public void pusherOut() {
-    	gearPusherOut.set(true);
-    	gearPusherIn.set(false);
+    	if (this.pusherIsBack()){
+    		gearPusherOut.set(true);
+        	gearPusherIn.set(false);
+    	}
+    	
     }
     
     public void pusherIn() {
-    	gearPusherOut.set(false);
-    	gearPusherIn.set(true);
+    	if (!this.pusherIsBack()){
+    		gearPusherOut.set(false);
+        	gearPusherIn.set(true);
+    	}
     }
+    
 }
 
