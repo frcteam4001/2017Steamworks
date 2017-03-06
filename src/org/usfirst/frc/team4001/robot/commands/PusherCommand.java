9002037a4da8a0006463887a2b9a4d9e19815ce0
JennyYang01 <jennyyang01@gmail.com>
2017-03-06 15:57:15 -0500

    Update PusherCommand

--- src/org/usfirst/frc/team4001/robot/commands/PusherCommand.java
+++ src/org/usfirst/frc/team4001/robot/commands/PusherCommand.java
@@ -1,4 +1,6 @@
-package org.usfirst.frc.team4001.robot.commands;
+ackage org.usfirst.frc.team4001.robot.commands;
+
+import java.awt.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -10,28 +12,32 @@ public class PusherCommand extends Command {
     public PusherCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	
+    	requires(Robot.pusher);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	System.out.println("PusherOut Init");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.pusher.pusherOut();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return !Robot.oi.game_controller.getRightAxisButton().get();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.pusher.pusherIn();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.pusher.pusherIn();
     }
 }
