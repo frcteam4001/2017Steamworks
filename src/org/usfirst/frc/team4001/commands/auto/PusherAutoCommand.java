140f9989fc9815a6067e6c14512351adafc50d32
JennyYang01 <jennyyang01@gmail.com>
2017-03-06 15:36:03 -0500

    Create PusherAutoCommand.java

---
+++ src/org/usfirst/frc/team4001/commands/auto/PusherAutoCommand.java
@@ -0,0 +1,35 @@
+*/
+public class PusherAutoCommand extends Command {
+
+    public PusherAutoCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.pusher);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	System.out.println("PusherOut Init");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.pusher.pusherOut();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.pusher.pusherIn();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.pusher.pusherIn();
+    }
+}
