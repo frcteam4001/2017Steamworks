3cee9950669f97547ec029580699fef738d5ce73
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-03-08 15:09:16 -0500

    auto updates

--- src/org/usfirst/frc/team4001/commands/auto/DriveAndGear.java
+++ src/org/usfirst/frc/team4001/commands/auto/DriveAndGear.java
@@ -18,21 +18,21 @@ public class DriveAndGear extends CommandGroup {
     	this.n = n;
     	// check for bumper dimensions
     	if(ln=="Key"){
-        	addSequential(new DriveCommand(12.6, 0.5, 0, 1, 0.2));
-        	addSequential(new DriveCommand(0, 0.5, 20*n, 1, 0.2));
+        	addSequential(new DriveCommand(39.19, 0.5, 0, 1, 0.2));
+        	addSequential(new DriveCommand(0, 0.5, 45*n, 1, 0.2));
 //orig        	addSequential(new DriveCommand(213.3, 0.5, 0, 3.5, 0.2));
  //       	addSequential(new DriveCommand(182.3, 0.5, 0, 3.5, 0.2));
-        	addSequential(new DriveCommand(150, 0.5, 0, 3.5, 0.2));
+        	addSequential(new DriveCommand(72.12, 0.5, 0, 3.5, 0.2));
 //orig        	addSequential(new DriveCommand(0, 0.5, (-140)*n, 1, 0.2));
-        	addSequential(new DriveCommand(0, 0.5, (-115)*n, 1, 0.2));
-        	addSequential(new DriveCommand(31.64, 0.5, 0, 1.5, 0.2));
+        	addSequential(new DriveCommand(0, 0.5, (-72)*n, 1, 0.2));
+        	addSequential(new DriveCommand(31.94, 0.5, 0, 1.5, 0.2));
     	}
     	else if(ln=="Ret"){
-    		addSequential(new DriveCommand(12.6, 0.5, 0, 1, 0.2));
-        	addSequential(new DriveCommand(0, 0.5, (-20)*n, 1, 0.2));
-        	addSequential(new DriveCommand(213.3, 0.5, 0, 3.5, 0.2));
-        	addSequential(new DriveCommand(0, 0.5, 140*n, 1, 0.2));
-        	addSequential(new DriveCommand(42.04, 0.5, 0, 1.5, 0.2));
+    		addSequential(new DriveCommand(39.19, 0.5, 0, 1, 0.2));
+        	addSequential(new DriveCommand(0, 0.5, (-29)*n, 1, 0.2));
+        	addSequential(new DriveCommand(72.12, 0.5, 0, 3.5, 0.2));
+        	addSequential(new DriveCommand(0, 0.5, 90*n, 1, 0.2));
+        	addSequential(new DriveCommand(22.29, 0.5, 0, 1.5, 0.2));
     	}
     	addSequential(new PlaceGear());
     	
